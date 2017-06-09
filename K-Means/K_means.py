20f8be92efd8d255b96df318f62e1649083990a4
GuoxianSong <songguoxian@icloud.com>
2017-06-09 18:14:16 +0800

    K-Means done.exclude "FindPlace",need vpn

--- K-Means/K_means.py
+++ K-Means/K_means.py
@@ -1,4 +1,6 @@
 from  numpy import *
+import urllib
+import json
 
 def LoadData(filename):
     dataMat = []
@@ -75,6 +77,34 @@ def biKmeans(dataSet,k, distMeas = distEclud):
 
 
 
-dataMat = LoadData("testSet2.txt")
-#randCent(dataMat,4)
-biKmeans(dataMat,3)
+#dataMat = LoadData("testSet2.txt")
+#biKmeans(dataMat,3)
+
+def geoGrab(stAddress, city):
+    apiStem = 'http://where.yahooapis.com/geocode?'  #create a dict and constants for the goecoder
+    params = {}
+    params['flags'] = 'J'#JSON return type
+    params['appid'] = 'aaa0VN6k'
+    params['location'] = '%s %s' % (stAddress, city)
+    url_params = urllib.urlencode(params)
+    yahooApi = apiStem + url_params      #print url_params
+    print yahooApi
+    c=urllib.urlopen(yahooApi)
+    return json.loads(c.read())
+
+from time import sleep
+def massPlaceFind(fileName):
+    fw = open('places.txt', 'w')
+    for line in open(fileName).readlines():
+        line = line.strip()
+        lineArr = line.split('\t')
+        retDict = geoGrab(lineArr[1], lineArr[2])
+        if retDict['ResultSet']['Error'] == 0:
+            lat = float(retDict['ResultSet']['Results'][0]['latitude'])
+            lng = float(retDict['ResultSet']['Results'][0]['longitude'])
+            print "%s\t%f\t%f" % (lineArr[0], lat, lng)
+            fw.write('%s\t%f\t%f\n' % (line, lat, lng))
+        else: print "error fetching"
+        sleep(1)
+    fw.close()
+geoGrab('1 VA Center','Augusta, ME')
